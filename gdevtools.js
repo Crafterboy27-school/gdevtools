e048173196942ac65ca77cc169bff9a3e4b39a66
Crafterboy27-school <95499245+Crafterboy27-school@users.noreply.github.com>
2024-10-25 15:24:54 +0000

    made into function

--- gdevtools.js
+++ gdevtools.js
@@ -1,64 +1,67 @@
-let win = window.open("about:blank")
-win.document.title = "GDevTools - "+window.location.href
-window.addEventListener("unload", function(){
-    win.close()
-})
-
-win.document.body.style.margin = '0'
-let tabDiv = win.document.createElement("div")
-tabDiv.style.cssText=`
-    width:100vw;
-    height:20px;
-    border-bottom:4px solid black;
-    padding-bottom:2px;
-`
-win.document.body.appendChild(tabDiv)
-
-let currentTabContainer = win.document.createElement("div")
-function addTab(name){
-    let tab = win.document.createElement("button")
-    tab.textContent = name;
-    tabDiv.appendChild(tab)
-
-    let tabContainer = win.document.createElement("div")
-    tabContainer.style.cssText = `
-        display: none;
-        width:100vw;
-        height:calc(100vh - 22px)
-        overflow-y: scroll;
-    `
-    win.document.body.appendChild(tabContainer)
-
-    tab.addEventListener("click", function(){
-        currentTabContainer.style.display = "none"
-        currentTabContainer = tabContainer
-        currentTabContainer.style.display = "block"
+const gdevtools = {opened:false}
+function OpenGDevTools(){
+    if(gdevtools.opened == true)return;
+    gdevtools.opened = true
+    gdevtools.win = window.open("about:blank")
+    gdevtools.win.document.title = "GDevTools - "+window.location.href
+    window.addEventListener("unload", function(){
+        gdevtools.win.close()
     })
 
-    return tabContainer
-}
-let consoleContainer = addTab("Console")
+    gdevtools.win.document.body.style.margin = '0'
+    let tabDiv = gdevtools.win.document.createElement("div")
+    tabDiv.style.cssText=`
+        width:100vw;
+        height:20px;
+        border-bottom:4px solid black;
+        padding-bottom:2px;
+    `
+    gdevtools.win.document.body.appendChild(tabDiv)
 
-const oldConsole = console
-const consoleProxyHandler = {
-    get(target, prop, receiver) {
-        if(prop=="log"||prop=="error"||prop=="warn"){
-            return function(...args){
-                let p = win.document.createElement("p")
-                p.textContent = JSON.stringify(args)
-                consoleContainer.appendChild(p)
+    let currentTabContainer = gdevtools.win.document.createElement("div")
+    function addTab(name){
+        let tab = gdevtools.win.document.createElement("button")
+        tab.textContent = name;
+        tabDiv.appendChild(tab)
 
-                oldConsole[prop](...args)
+        let tabContainer = gdevtools.win.document.createElement("div")
+        tabContainer.style.cssText = `
+            display: none;
+            width:100vw;
+            height:calc(100vh - 22px)
+            overflow-y: scroll;
+        `
+        gdevtools.win.document.body.appendChild(tabContainer)
+
+        tab.addEventListener("click", function(){
+            currentTabContainer.style.display = "none"
+            currentTabContainer = tabContainer
+            currentTabContainer.style.display = "block"
+        })
+
+        return tabContainer
+    }
+    let consoleContainer = addTab("Console")
+
+    const oldConsole = console
+    const consoleProxyHandler = {
+        get(target, prop, receiver) {
+            if(prop=="log"||prop=="error"||prop=="warn"){
+                return function(...args){
+                    let p = gdevtools.win.document.createElement("p")
+                    p.textContent = JSON.stringify(args)
+                    consoleContainer.appendChild(p)
+
+                    oldConsole[prop](...args)
+                }
             }
-        }
-        return Reflect.get(...arguments);
-    },
-};
+            return Reflect.get(...arguments);
+        },
+    };
 
-window.addEventListener("error", function(e){
-    console.error(e.error.message)
-})
-  
-console = new Proxy(oldConsole, consoleProxyHandler);
-
-a.b = 0
+    window.addEventListener("error", function(e){
+        console.error(e.error.message)
+    })
+    
+    console = new Proxy(oldConsole, consoleProxyHandler);
+}
